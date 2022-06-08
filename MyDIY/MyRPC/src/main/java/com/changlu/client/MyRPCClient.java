8e094d97ce69f275dfa1086f41c29d1e38f1f35b
changlu <939974883@qq.com>
2022-06-08 16:20:28 +0800

    新增MyRPC的README.md

--- MyDIY/MyRPC/src/main/java/com/changlu/client/MyRPCClient.java
+++ MyDIY/MyRPC/src/main/java/com/changlu/client/MyRPCClient.java
@@ -18,27 +18,22 @@ public class MyRPCClient {
         RPCClientProxy rpcClientProxy = new RPCClientProxy(
                 new NettyRPCClient(new RoundLoadBalance())//选择指定的策略
         );//不指定IP地址及端口号，就通过使用服务发现来获取ip地址
-//        for (int i = 0; i < 5; i++) {
         try {
             //1、调用用户业务类
             UserService userService = rpcClientProxy.getRPCProxy(UserService.class);
             System.out.println(userService.getUserByUserId(123));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            Thread.sleep(10 * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        //2、调用博客业务类
-        try {
+            //测试心跳检测
+            try {
+                Thread.sleep(10 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //2、调用博客业务类
             BlogService blogService = rpcClientProxy.getRPCProxy(BlogService.class);
             System.out.println(blogService.getBlogById(123));
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        }
     }
 
 }
