93e733f046a75d8bc98cd9d660beeed35b053138
changlu <939974883@qq.com>
2022-07-12 11:18:13 +0800

    update

--- algorithm-java/src/main/java/com/changlu/java/Test.java
+++ algorithm-java/src/main/java/com/changlu/java/Test.java
@@ -1,9 +1,29 @@
 package com.changlu.java;
 
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+
 /**
  * @Description:
  * @Author: changlu
  * @Date: 4:27 PM
  */
 public class Test {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        System.out.println((num1 ^ num2) ^ num2);
+        ArrayList<TreeNode> temp = new ArrayList<>();
+        temp.size()
+    }
+
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
 }
